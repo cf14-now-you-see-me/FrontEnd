f3fb09839fe4c8ea0cfbb3306c3ba1d54ad403ee
harismau <haris.maulana91@ui.ac.id>
2022-09-04 16:53:36 +0700

    revisi pemesanan total

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,11 +2,7 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
 import './NavbarNew.css';
-<<<<<<< HEAD
-import logokerinci from '../assets/kerinci22.png';
-=======
 import logo from '../assets/LogoNew.png';
->>>>>>> 274303d786772b917f55467e24da3eb3a9646988
 import burger from '../assets/burger.svg';
 import {useNavigate} from 'react-router-dom';
 
@@ -18,34 +14,6 @@ const Navbar = ({setShow, size}) => {
   // const [openLinks, setOpenLinks] = useState(false) //initially state = false
 
   return (
-<<<<<<< HEAD
-    <div className="navbar">
-      <div className="navbar-left">
-        <img src={logokerinci} style={{height: '40px'}} alt="" />
-        <h2 className="logo">Kerinci 22</h2>
-      </div>
-      <div className="navbar-center">
-        <Link className="navbar-text" to="/">
-          Beranda
-        </Link>
-        <Link className="navbar-text" to="/rekomendasi">
-          Rekomendasi
-        </Link>
-        <Link className="navbar-text" to="/reservasi">
-          Reservasi
-        </Link>
-        {/* <Link className="navbar-text" to="/kontak">
-          Kontak
-        </Link> */}
-      </div>
-      <div className="navbar-right">
-        {/* <Link className="navbar-text" to="/search">
-          Search
-        </Link> */}
-        <Link className="navbar-text2" to="/profil">
-          Halo, Anonim
-        </Link>
-=======
     <div className='navbar'>
       <div className='navbar-left'>
       <img src={logo} />
@@ -59,8 +27,8 @@ const Navbar = ({setShow, size}) => {
         {/* <div className='navbar-text' onClick={() => setShow(true)}>Reservasi</div> */}
         {/* <Link to="/booking"> */}
         <div className='cart' onClick={() => setShow(false)}>
-          <p className='navbar-text'>Reservasi</p>
-          <span>{size}</span>
+          <p className='navbar-text'>Reservasi ({size})</p>
+          {/* <span>{size}</span> */}
         </div>
         {/* </Link> */}
 
@@ -68,7 +36,6 @@ const Navbar = ({setShow, size}) => {
       <div className='navbar-right'>
         {/* <Link className='navbar-text' to="/search"> Search </Link> */}
         <Link className='navbar-text2' to="/profil"> Halo, Anonim </Link>
->>>>>>> 274303d786772b917f55467e24da3eb3a9646988
       </div>
     </div>
   );
